26f3509dc7c3eb0b2fffaf9f6b089eb30447b15f
Nikin Nagewadia <nikin.n@gmail.com>
2015-01-18 22:00:12 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+	//your jQuery here
+	$('.menu-button').click(function (){
+		$('#site-menu').show('slow');
+		
+	});
+
+	$('.exit-button').click(function (){
+		$('#site-menu').hide('slow');
+		
+	});
+
+});
