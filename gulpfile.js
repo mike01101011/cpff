88e76e1ee37114e80fb16ee06ac7ac8d819af435
Nikin Nagewadia <nikin.n@gmail.com>
2015-02-07 14:37:08 -0500

    more upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,29 @@
 var gulp = require('gulp'),
 
 // Required Gulp Plugins
+	plumber = require('gulp-plumber'),
 	sass = require('gulp-sass'),
-	// livereload = require('gulp-livereload'),
 	browserSync = require('browser-sync'),
 	reload = browserSync.reload,
 	autoprefixer = require('gulp-autoprefixer'),
-	// sourcemaps = require('gulp-sourcemaps'),
 	minifyCSS = require('gulp-minify-css'),
-	imagemin = require('gulp-imagemin'),
-	plumber = require('gulp-plumber');
+	imagemin = require('gulp-imagemin');
 
-// Static Server
+// Proxy Server
 gulp.task('browser-sync', function() {
 	browserSync({
 		proxy: "localhost:8888/cpff/"
 	});
 });
 
-// Styles
-// Compiles SASS to CSS
+// Styles - Compiles SASS to CSS
 gulp.task('styles', function(){
 	return gulp.src('./sass/*.scss')
-		.pipe(sass())
 		.pipe(plumber())
+		.pipe(sass())
 		.pipe(autoprefixer({
 			browsers: ['last 5 version']
 		}))
-		// .pipe(sourcemaps.init())
-		// .pipe(sourcemaps.write('./sass/maps'))
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('./'))
 		.pipe(reload({stream:true}));
@@ -43,7 +38,11 @@ gulp.task('images', function() {
 		.pipe(gulp.dest('./img/'))
 });
 
-// Default
-gulp.task('default', ['styles', 'images', 'browser-sync'], function () {
+gulp.task('watch', function() {
 	gulp.watch('./sass/*.scss', ['styles']);
+	gulp.watch('./img/*.*', ['images']);
+
 });
+
+// Default
+gulp.task('default', ['styles', 'images', 'browser-sync', 'watch']);
