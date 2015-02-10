0557c825b917f8946d6f3d05fe360f116cb3eb9d
Nikin Nagewadia <nikin.n@gmail.com>
2015-02-09 23:14:18 -0500

    body lock

--- carousel.php
+++ carousel.php
@@ -1,7 +1,7 @@
   <main>
   	<section class="overlay-carousel">	
 	  <div class="carousel">
-	    <div > <img src="http://lorempixel.com/output/city-q-c-1440-500-7.jpg" alt="test"> </div>
+	    <div> <img src="http://placehold.it/1440x500" alt="test"> </div>
 	    <div> <img src="http://placehold.it/1440x500" alt="test"> </div>
 	  </div>
   	</section> <!-- /.page-carousel -->
--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,11 @@ $(function(){
 	
 	$('.menu-button').click(function (){
 		$('#site-menu').css('right', '0');
+		$('body').css('overflow','hidden');
 		$('main').css({
 			'background':'rgba(0,0,0,0.75)',
 			'z-index':'100',
-			'transition':'all 0.75s'
+			'transition':'all 0.75s',
 		});
 		$('footer').css({
 			'background':'rgba(0,0,0,0.75)',
@@ -17,6 +18,7 @@ $(function(){
 	});
 	$('.exit-button').click(function (){
 		$('#site-menu').css('right', '-100%');
+		$('body').css('overflow','visible');
 		$('main').css({
 			'background':'white',
 			'z-index':'0',
