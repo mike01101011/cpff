2ad1773c234ec2a9efc0d9176145ff1ea0102109
Nikin Nagewadia <nikin.n@gmail.com>
2015-02-14 15:11:31 -0500

    cpff logo insert

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(function(){
 	//your jQuery here
-	
 	$('.menu-button').click(function (){
 		$('#site-menu').css('right', '0');
 		$('body').css('overflow','hidden');
@@ -15,7 +14,9 @@ $(function(){
 			'transition':'all 0.75s'
 		});
 		$('.carousel').css('opacity', '.25');
+		$('.identity').css('opacity', '.25');
 	});
+
 	$('.exit-button').click(function (){
 		$('#site-menu').css('right', '-100%');
 		$('body').css('overflow','visible');
@@ -30,6 +31,29 @@ $(function(){
 			'transition':'all 0.75s'
 		});
 		$('.carousel').css('opacity', '1');
+		$('.identity').css('opacity', '1');
 	});
-	
+
+	var windw = this;
+	$.fn.followTo = function ( pos ) {
+	    var $this = this,
+	        $window = $(windw);
+	    
+	    $window.scroll(function(e){
+	        if ($window.scrollTop() > pos) {
+	            $this.css({
+	                position: 'absolute',
+	                top: pos
+	            });
+	        } else {
+	            $this.css({
+	                position: 'fixed',
+	                top: 0
+	            });
+	        }
+	    });
+	};
+	$('.identity').followTo(300);
+	// figure out the percentage value of the height of carousel at different widths
+
 });
