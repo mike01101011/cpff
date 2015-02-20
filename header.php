09586b103cf0bb79590f3efaea9bcfabda61ba56
Nikin Nagewadia <nikin.n@gmail.com>
2015-02-19 20:14:20 -0500

    update fontawesome

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
   <link href='http://fonts.googleapis.com/css?family=Montserrat:700,400|Open+Sans:400italic,400' rel='stylesheet' type='text/css'>
-  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
   <?php // Load our CSS ?>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
