27b2ca12f0f26a0ec7ab8b35143b1e1db9cb29cd
Nikin Nagewadia <nikin.n@gmail.com>
2015-03-28 18:42:41 -0400

    dropshadow to svgs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(function(){
 	//your jQuery here
+	
+	//When click menu button, show menu
 	$('.menu-button').click(function() {
 		$('nav').addClass('open');
 		$('body').css('overflow','hidden');
+		
+		//rotating the reel when menu opens using Greensock Animation
+		var reel = document.getElementsByClassName("menu-reel")
+		TweenMax.to(reel, '3', {
+		  delay: '1.5',
+		  rotation: '360',
+		  transformOrigin: '50% 50%',
+		  
+		});
 	});
 
+	//When click on exit button, close menu button
 	$('.exit-button').click(function() {
 		$('nav').removeClass('open');
 		$('body').css('overflow','visible');
@@ -12,12 +24,16 @@ $(function(){
 
 });
 
+
+// Greensock Animation:
+// - Animating Text
 var text = document.getElementsByClassName("text")
 TweenMax.to(text, '1', {
   delay: '1',
   opacity: '1',
 });
 
+// - Animating the Reel
 var reel = document.getElementsByClassName("reel")
 TweenMax.to(reel, '3', {
   delay: '2', 
