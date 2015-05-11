40ff6abe195c0d37159265d1e587e8a7baf6d2e8
Nikin Nagewadia <nikin.n@gmail.com>
2015-05-11 10:33:44 -0400

    before launch

--- footer.php
+++ footer.php
@@ -9,20 +9,19 @@
 	  </div>
 	</footer>
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.0/TweenMax.min.js"></script>	
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.1/slick.min.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.16.0/TweenMax.min.js"></script>	
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.1/slick.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
 
-
-<script>
-/* Google Analytics! */
- var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
- (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
- g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
- s.parentNode.insertBefore(g,s)}(document,"script"));
-</script>
+	<script>
+	/* Google Analytics! */
+	 var _gaq=[["_setAccount","UA-51002902-1"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
+	 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	 g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+	 s.parentNode.insertBefore(g,s)}(document,"script"));
+	</script>
 
 
 </body>
