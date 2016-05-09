ab334cf3697f965dc38d2c844e3ab9a9b31202df
mike01101011 <mike01101011@gmail.com>
2016-05-09 01:23:24 -0400

    films and film page css update

---
+++ page-films-coming-soon.php
@@ -0,0 +1,37 @@
+<!-- website header - start -->
+<?php get_header(); ?>
+<!-- website header - end -->
+
+
+<!-- website navigation - start -->
+<?php include("navigation.php"); ?>
+<!-- website navigation - end -->
+
+
+<!-- carousel - start -->
+<? include("carousel.php"); ?>
+<!-- carousel - end -->
+
+
+<!-- website main content - start -->
+<main class="clearfix">
+
+	<div class="container">
+		<div class="title-headline">
+			<div class="title-headline-flex coming-soon-title-headline">
+				<div class="title-headline-flex-left">
+					<!-- page title - start -->
+					<h2>COMING SOON</h2>
+					<!-- page title - end -->				
+				</div> <!-- .title-headline-left -->
+			</div> <!-- .title-headline-flex -->
+		</div>  <!-- /.title-headline -->	
+	</div> <!-- .container -->
+		
+</main> <!-- /.clearfix -->
+<!-- website main content - end -->
+
+
+<!-- website footer - start -->
+<?php get_footer(); ?>
+<!-- website footer - end
